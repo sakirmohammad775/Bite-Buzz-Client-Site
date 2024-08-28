3fec2ebf6ddf21587f6c25dec4008bccd7fd27e8
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-28 23:32:35 +0600

    add  section

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -5,7 +5,7 @@ const SectionTitle = ({heading ,subHeading}) => {
         <>
         <div className="mx-auto text-center md:w-3/12 my-8">
             <p className="text-yellow-600 mb-2">---{subHeading}---</p>
-            <p className="text-4xl uppercase border-y-4 py-4">{heading}</p>
+            
         </div>
         </>
     );
