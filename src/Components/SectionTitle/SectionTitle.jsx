3c5eba4dff7b422ce47e969e785f5f5a323bd500
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-29 19:56:48 +0600

    add  section

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -7,6 +7,7 @@ const SectionTitle = ({heading ,subHeading}) => {
             <p className="text-yellow-600 mb-2">---{subHeading}---</p>
             <p className="text-4xl uppercase border-y-4 py-4">{heading}</p>
         </div>
+        <h3>hello Bangladesh</h3>
         </>
     );
 };
