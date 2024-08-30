b78d1deb467100247ac5630988dbb45ba160a185
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-30 20:23:44 +0600

    add  section

---
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,15 @@
+
+
+const SectionTitle = ({heading ,subHeading}) => {
+    return (
+        <>
+        <div className="mx-auto text-center md:w-3/12 my-8">
+            <p className="text-yellow-600 mb-2">---{subHeading}---</p>
+            <p className="text-4xl uppercase border-y-4 py-4">{heading}</p>
+        </div>
+        <h2>Bangladesh</h2>
+        </>
+    );
+};
+
+export default SectionTitle;
