462fb10fa6a13995b7857e0e6cb1532efc1ee659
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-31 01:08:38 +0600

    add menuitem page int he section

--- src/Pages/Shared/MenuItem/MenuItem.jsx
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -1,8 +1,14 @@
 
 const MenuItem = ({item}) => {
+    const {name,image ,price,recipe}=item
     return (
-        <div>
-            
+        <div className="flex space-x-4">
+            <img style={{bo}} className="w-[100px] rounded-3xl" src={image} alt="" />
+            <div>
+                <h3 className="uppercase">{name}--------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className="text-yellow-500">${price}</p>
         </div>
     );
 };
