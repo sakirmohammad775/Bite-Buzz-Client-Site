b2e9e4c89ee86e4ea43382964dabadc26a6491f9
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-08-31 23:31:23 +0600

    parallax setup

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -3,14 +3,17 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 import { useEffect, useState } from 'react';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const Testimonials = () => {
     const [reviews, setReviews] = useState([])
+    const [rating, setRating] = useState(0)
     useEffect(() => {
         fetch('reviews.json')
             .then(res => res.json())
             .then(data => setReviews(data))
-    },[])
+    }, [])
     return (
         <><Swiper navigation={true} modules={[Navigation]} className="mySwiper">
 
@@ -18,8 +21,12 @@ const Testimonials = () => {
                 <SwiperSlide key={review._id}>
 
                     <div>
-                        <p>{review.details}
-                        </p>
+                        <p>{review.details}</p>
+                        <h3 className='text-2xl text-orange-400'>{review.name}</h3>
+                        <Rating
+                            style={{ maxWidth: 250 }}
+                            value={rating}
+                            onChange={setRating} />
                     </div>
                 </SwiperSlide>)}
 
