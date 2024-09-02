b6740fc9648dcd27dd6366bf87ec4d6ad3e4f773
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-02 13:13:25 +0600

    using customhook

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
                             {navOptions}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">BiteBuzz</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
