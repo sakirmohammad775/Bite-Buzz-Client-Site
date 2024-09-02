bad0b81882f6179af3a9585536504954b85acf85
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-02 14:19:48 +0600

    add tab in project

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -2,23 +2,25 @@ import CoverImg from "../../assets/shop/banner2.jpg"
 import Cover from "../Shared/Cover/Cover"
 import 'react-tabs/style/react-tabs.css';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { useState } from "react";
 const Order = () => {
+    const [tabIndex,setTabIndex]=useState(0)
     return (
         <>
-        <Cover img={CoverImg} title={"OUR SHOP"}></Cover>
-        <Tabs>
-    <TabList>
-      <Tab>Title 1</Tab>
-      <Tab>Title 2</Tab>
-    </TabList>
+            <Cover img={CoverImg} title={"OUR SHOP"}></Cover>
+            <Tabs defaultIndex={1} onSelect={(index)}>
+                <TabList>
+                    <Tab>Pizza</Tab>
+                    <Tab>Title 2</Tab>
+                </TabList>
 
-    <TabPanel>
-      <h2>Any content 1</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>Any content 2</h2>
-    </TabPanel>
-  </Tabs>
+                <TabPanel>
+                    <h2>Any content 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Any content 2</h2>
+                </TabPanel>
+            </Tabs>
         </>
     );
 };
