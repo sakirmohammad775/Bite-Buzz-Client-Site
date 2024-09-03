5a20c627feb6f1265a47448e8b63d91598022be1
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-03 13:25:16 +0600

    add tab in project

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -15,7 +15,7 @@ const FoodCard = ({item}) => {
     <h2 className="card-title">Shoes!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
     <div className="card-actions">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-primary">Order Now</button>
     </div>
   </div>
 </div>
