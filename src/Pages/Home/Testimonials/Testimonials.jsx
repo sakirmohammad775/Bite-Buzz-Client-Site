9e724b019df34427f38b0229d81f92611220e4cf
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-07 22:52:10 +0600

    add to the card

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -11,7 +11,7 @@ const Testimonials = () => {
     const [reviews, setReviews] = useState([])
     const [rating, setRating] = useState(0)
     useEffect(() => {
-        fetch('reviews.json')
+        fetch('http://localhost:5000/review')
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [])
@@ -30,9 +30,7 @@ const Testimonials = () => {
                             <h3 className='text-2xl text-orange-400'>{review.name}</h3>
                         </div>
        <div>
-        <h4 className='text-4xl items-center font-thin'>
-            ht is the liquaid hangsterll 
-        </h4>
+       
        </div>
 
                     </SwiperSlide>)}
