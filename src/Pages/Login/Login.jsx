6f5bf6dc86fc97b7e099a9f8ca41e7ce020d0fba
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-08 23:13:56 +0600

    add login in the project

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault()
+        const form=event.target
+        const email=form.email.value
+        const password =form.password.value
+        console.log(email,password)
+    }
     return (
         <>
             <div className="hero bg-base-200 min-h-screen">
@@ -13,24 +20,25 @@ const Login = () => {
                         </p>
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                        <form className="card-body">
+                        <form className="card-body" onSubmit={handleLogin}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" placeholder="email" className="input input-bordered" name='email' required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" placeholder="password"
+                                    name="password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                                <input type="submit" className='btn btn-primary'   />
                             </div>
                         </form>
                     </div>
