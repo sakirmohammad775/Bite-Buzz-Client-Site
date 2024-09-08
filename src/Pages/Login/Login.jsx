1da7de1b6c6e54d84d9b41e42208cb130e8ad24b
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-08 23:14:14 +0600

    add login in the project

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -14,10 +14,7 @@ const Login = () => {
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
+                       
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                         <form className="card-body" onSubmit={handleLogin}>
