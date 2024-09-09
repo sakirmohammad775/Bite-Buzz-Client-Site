3a3f288fa58c66a861cc895afc5c63b0330747d0
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-09 15:32:33 +0600

    add authprovider,local env,firebase etc

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,21 +1,33 @@
-import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "../../firebase.config";
 
-const AuthContext=createContext(null)
-const auth=getAuth(app)
+const AuthContext = createContext(null)
+const auth = getAuth(app)
 
-const AuthProvider=({children})=>{
-    const [user,setUser]=useState(null)
-    const[loading,setLoading]=useState(true)
-    const authInfo={
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('current user', currentUser)
+            setLoading(false)
+        });
+        return () => {
+            return unsubscribe()
+        }
+    }, [])
+    
+    const authInfo = {
         user,
         loading
     }
 }
-return(
+return (
     <AuthContext.Provider value={authInfo}>
-            {children}
+        {children}
     </AuthContext.Provider>
 )
 export default AuthContext
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes';
-import {  HelmetProvider } from 'react-helmet-async'
+import { HelmetProvider } from 'react-helmet-async'
+import AuthContext from './Providers/AuthProvider';
 
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <AuthContext>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </AuthContext>
   </StrictMode>,
 
 )
