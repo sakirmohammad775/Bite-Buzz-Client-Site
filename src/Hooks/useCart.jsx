a4638c067b3f360b969ac06ff116f759243f7b39
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-16 21:39:02 +0600

    add the axos

--- src/Hooks/useCart.jsx
+++ src/Hooks/useCart.jsx
@@ -11,7 +11,7 @@ const useCart = () => {
     const { data: cart = [] } = useQuery({
         queryKey: ['cart',user?.email],
         queryFn: async () => {
-            const res = await axiosSecure.get(`/carts?email${user.email}`)
+            const res = await axiosSecure.get(`/carts?email=${user.email}`)
             return res.data
         }
     })
