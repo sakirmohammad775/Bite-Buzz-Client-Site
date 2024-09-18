f4c624766593edb6ee6e74cf93158910703b1c5d
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-18 10:29:51 +0600

    check the accuracy

--- src/MainLayout/DashboardLayout.jsx
+++ src/MainLayout/DashboardLayout.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { FaAd, FaHome, FaList, FaPaypal, FaShoppingCart, FaVoicemail } from 'react-icons/fa';
+import { FaCartShopping, FaShop, FaTowerObservation } from 'react-icons/fa6';
+import { NavLink, Outlet } from 'react-router-dom';
 
 const DashboardLayout = () => {
     return (
-        <div>
-            
+        <div className="max-w-screen-xl mx-auto flex">
+            {/* dashboard side bar */}
+            <div className='w-64 min-h-screen bg-orange-400 text-black'>
+                <ul className='menu p-4'>
+                    <li><NavLink to='/dashboard/cart'><FaHome></FaHome>User Home</NavLink></li>
+                    <li><NavLink to='/dashboard/reservation'><FaTowerObservation></FaTowerObservation>Reservation</NavLink></li>
+                    <li><NavLink to='/dashboard/cart'><FaCartShopping></FaCartShopping> My Cart</NavLink></li>
+                    <li><NavLink to='/dashboard/payment'><FaPaypal></FaPaypal> Payment History</NavLink></li>
+                    <li><NavLink to='/dashboard/review'><FaAd></FaAd> Add a Review</NavLink></li>
+                    <li><NavLink to='/dashboard/bookings'><FaList></FaList> My Bookings</NavLink></li>
+
+                    <div className="divider "></div>
+
+                    <li><NavLink to='/'><FaHome></FaHome> Home</NavLink></li>
+                    <li><NavLink to='/menu'><FaList></FaList> Menu</NavLink></li>
+                    <li><NavLink to='/order'><FaShop></FaShop> Shop</NavLink></li>
+                    <li><NavLink to='/Contact'><FaVoicemail></FaVoicemail>Contact</NavLink></li>
+                </ul>
+            </div>
+            {/* dashboard content */}
+            <div className='flex-1 bg-red-700 h-8'>
+                <Outlet></Outlet>
+            </div>
         </div>
     );
 };
