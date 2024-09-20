2f95cb2b73d5eea051627288d18d94140812c226
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-20 12:25:36 +0600

    add google signIn

--- src/Components/SocialLogin/SocialLogin.jsx
+++ src/Components/SocialLogin/SocialLogin.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { FaGoogle } from 'react-icons/fa';
 import useAuth from '../../Hooks/useAuth';
 import useAxiosPublic from '../../Hooks/useAxiosPublic';
+import { useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
     const { googleSignIn } = useAuth()
     const axiosPublic = useAxiosPublic()
+    const navigate=useNavigate()
 
     const handleGoogleSignIn = () => {
         googleSignIn()
@@ -15,6 +17,11 @@ const SocialLogin = () => {
                     email: result.user?.email,
                     name: result.user?.displayName
                 }
+                axiosPublic.post('/users',userInfo)
+                .then(res=>{
+                    console.log(res.data)
+                    navigate('/')
+                })
             })
     }
     return (
