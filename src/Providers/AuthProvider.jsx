933df3ad43f4c87cbe9f6aad5df8cdbe76caa012
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-22 21:13:16 +0600

    add the jwt to my site

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile, } from "firebase/auth";
 import { app } from "../../firebase.config";
+import useAxiosPublic from "../Hooks/useAxiosPublic"
 
 
 export const AuthContext = createContext(null)
@@ -9,7 +10,8 @@ const auth = getAuth(app)
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
-    const googleProvider=new GoogleAuthProvider()
+    const googleProvider = new GoogleAuthProvider()
+    const axiosPublic = useAxiosPublic()
 
     const createUser = (email, password) => {
         setLoading(true)
@@ -20,10 +22,10 @@ const AuthProvider = ({ children }) => {
         setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
-    
-    const googleSignIn=()=>{
+
+    const googleSignIn = () => {
         setLoading(true)
-        return signInWithPopup(auth,googleProvider)
+        return signInWithPopup(auth, googleProvider)
     }
 
     const logOut = () => {
@@ -43,6 +45,20 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('current user', currentUser)
+            if (currentUser) {
+                //get token and store client
+                const userInfo = { email: currentUser.email }
+                axiosPublic.post('/jwt',userInfo)
+                .then(res=>{
+                    if(res.data.token){
+                        localStorage.setItem('access-token',res.data.token)
+                    }
+                })
+            }
+            else {
+                //todo something:remove token(if token stored in the client side:local storage,catching,in memory)
+                localStorage.removeItem('access-token')
+            }
             setLoading(false)
         });
         return () => {
