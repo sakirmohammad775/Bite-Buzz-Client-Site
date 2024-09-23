bf03dac34e7bf950b3d110fcfe91a3394cc4d695
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-24 00:38:07 +0600

    ad the interceptors

--- src/Hooks/useAdmin.jsx
+++ src/Hooks/useAdmin.jsx
@@ -3,14 +3,14 @@ import useAuth from './useAuth';
 import useAxiosSecure from './useAxiosSecure';
 
 const useAdmin = () => {
-    const {user}=useAuth()
-    const axiosSecure=useAxiosSecure
-    const {data:isAdmin}=useQuery({
-        queryKey:[user?.email,'isAdmin'],
-        queryFn:async()=>{
-            const res=await axiosSecure.get(`/users/admin/${user.email}`)
+    const { user } = useAuth()
+    const axiosSecure = useAxiosSecure()
+    const { data: isAdmin } = useQuery({
+        queryKey: [user?.email, 'isAdmin'],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/users/admin/${user.email}`)
             console.log(res.data)
-            return res.data?.admin
+            return res.data?.admin;
         }
     })
     return [isAdmin]
