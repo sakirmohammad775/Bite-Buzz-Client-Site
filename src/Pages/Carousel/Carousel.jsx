f26258997ea648505ed7194598009d46ea98d5af
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-27 22:29:27 +0600

    add imgbb photo caarousel

--- src/Pages/Carousel/Carousel.jsx
+++ src/Pages/Carousel/Carousel.jsx
@@ -34,6 +34,7 @@ const Carousel = () => {
         <h1 className="text-5xl font-bold">AUSTRALIA</h1>
         <p className="mt-4 text-lg">Lorem ipsum dolor,I sit amet consectetur adipisicing elit. Ab, eum!</p>
         <p className="mt-4 text-lg">Lorem ipsum dolor,I sit amet consectetur adipisicing elit. Ab, eum!</p>
+        <p className="mt-4 text-lg">Lorem ipsum dolor,I sit amet consectetur adipisicing elit. Ab, eum!</p>
         <button className="mt-4 px-6 py-2 bg-white text-gray-900 font-semibold rounded">See More</button>
       </div>
 
