3df44630410816b0e4f5ae805c345322ec44b0a1
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-12 09:34:55 +0600

    add image to the imagebb

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -10,9 +10,16 @@ const image_hosting_api=`https://api.imgbb.com/1/upload?key=${image_hosting_key}
 const AddItems = () => {
     const { register, handleSubmit } = useForm()
     const axiosPublic=useAxiosPublic()
-    const onSubmit = (data) =>{
+    const onSubmit = async(data) =>{
         console.log(data)
         //image upload to imgbb and then get an url
+        const imageFile={image:data.image[0]}
+        const res = await axiosPublic.post(image_hosting_api,imageFile,{
+            headers:{
+                'Content-Type': 'multipart/form-data',
+            }
+        });
+        console.log(res.data)
     }
     return (
         <div>
