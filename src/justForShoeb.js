b4bc95af7c59d34b1c929eaf12c646510555ed65
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-16 23:49:50 +0600

    add the new item

---
+++ src/justForShoeb.js
@@ -0,0 +1,20 @@
+/*
+ 
+                                
+                                <label className="label">
+                                    <span className="label-text">Category</span>
+                                </label>
+                                <select defaultValue="default" {...register("category")} className="select select-bordered w-full" >
+                                    <option disabled value="default">Category</option>
+                                    <option value="pizza">Pizza</option>
+                                    <option value="salad">Salad</option>
+                                    <option value="soup">Soup</option>
+                                    <option value="dessert">Dessert</option>
+                                    <option value="drinks">Drinks</option>
+*/
+/* 
+git add .
+git commit -m "add the new item"
+git push -u origin main
+
+*/
